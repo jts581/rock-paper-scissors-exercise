7310fbcb33f5547d3e05028e4884508a842fb691
jts581 <jts581@stern.nyu.edu>
2021-05-26 18:58:36 -0400

    Winner determination working

--- game.py
+++ game.py
@@ -24,6 +24,10 @@ valid_options = ["rock","paper","scissors"]
 computer_choice = random.choice(valid_options)
 print("COMPUTER CHOICE:",computer_choice)
 
-
+# determine the winner
+if (user_choice == "rock" and computer_choice == "paper") or (user_choice == "paper" and computer_choice == "scissors") or (user_choice == "scissors" and computer_choice == "rock"):
+    print("COMPUTER WINS, YOU LOSE!")
+else:
+    print("YOU WIN, COMPUTER LOSES")
 
 print("THIS IS THE END OF OUR GAME, PLEASE PLAY AGAIN")
